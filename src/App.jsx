cc9be35b427a524999528b6bbbd7d39b6f0178f8
Dev41 <dev41@example.com>
2025-05-19 12:58:22 +0530

    Initialised react project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-3xl font-bold underline text-red-500 text-center'>Hello World</div>
+  )
+}
+
+export default App
