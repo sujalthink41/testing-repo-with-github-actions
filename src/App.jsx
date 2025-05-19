4c6eebb662c26687d1b5c1f27158c80c07ee0567
Dev41 <dev41@example.com>
2025-05-19 13:25:01 +0530

    Added github workflow file and minor changes in the app.js

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const App = () => {
   return (
+    <>
     <div className='text-3xl font-bold underline text-red-500 text-center'>Hello World</div>
+    <p className='text-center text-lg text-gray-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam expedita odit est blanditiis adipisci? Facilis perspiciatis reprehenderit iusto quos ex deleniti rem excepturi autem. Iusto eos eius nisi quia obcaecati at, minima cum atque corrupti, officiis debitis voluptatum ad perferendis exercitationem suscipit nihil dolorem voluptas dolorum doloremque adipisci sit voluptates!</p>
+    </>
+    
   )
 }
 
